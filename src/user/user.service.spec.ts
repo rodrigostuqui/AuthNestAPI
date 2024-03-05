5890fa7db136cc47c6295e3076d7a4e305f99728
Rodrigo Stuqui Monzani <rst.monzani@gmail.com>
2024-03-05 09:46:39 -0400

    Feat(User): Criação do user e integração com prisma

---
+++ src/user/user.service.spec.ts
@@ -0,0 +1,17 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { UserService } from './user.service';
+import { PrismaService } from '../prisma/prisma.service';
+
+describe('UserService', () => {
+  let service: UserService;
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [UserService, PrismaService], // Include PrismaService provider
+    }).compile();
+    service = module.get<UserService>(UserService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+});
